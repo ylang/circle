6a3f314865379f0d935947445836ace3b66d62a6
ylang <ylang@andrew.cmu.edu>
2013-03-05 03:31:36 -0500

    change to nodejs back

---
+++ web.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+console.log("hello");
+function init() {
+    var app = express();
+    configureExpress(app);
+    mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/myApp');
+    http.createServer(app).listen(process.env.PORT || 91419, function() {
+        console.log("Express server listening on port %d", process.env.PORT || 91419);
+    });
+
+
+}
+
+init();
+
+function configureExpress(app) {
+    app.configure(function() {
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+
+        app.use(express.cookieParser('your secret here'));
+        app.use(express.session());
+
+        app.use(app.router);
+        app.use(express["static"](__dirname + "/www/"));
+    });
+}
